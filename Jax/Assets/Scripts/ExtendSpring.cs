4c151ef599c03a62f66e71025fe1a4cf40cd67d6
Rienzi Gokea <Gamma169@github.com>
2016-01-23 17:17:50 -0500

    Setup First Scenes
    Built the regular spring and some of the other spring leg design

---
+++ Jax/Assets/Scripts/ExtendSpring.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtendSpring : MonoBehaviour {
+
+	public float regSpringLength = 3f;
+	public float extSpringLength = 5f;
+
+	public SpringJoint2D spring;
+
+	// Use this for initialization
+	void Start () {
+		spring = GetComponent<SpringJoint2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("space") )
+			spring.distance = extSpringLength;
+		else
+			spring.distance = regSpringLength;
+
+
+
+
+
+		/*
+		if (Input.GetKey ("space") && spring.distance <= extSpringLength)
+			spring.distance = spring.distance + .05f;
+		if (!Input.GetKey ("space") && spring.distance >= regSpringLength )
+			spring.distance = spring.distance - .05f;
+		*/
+	}
+}
