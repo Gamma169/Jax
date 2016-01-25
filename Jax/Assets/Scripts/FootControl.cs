cbec76db567386e1327127dcf62f9e0e18089c79
Rienzi Gokea <Gamma169@github.com>
2016-01-25 00:09:57 -0500

    Finished Angle Force Control Simple Spring -  Having Little Trouble with Angle Calculations
    Everything is just about set for the simple spring simulation.  I'm
    having some trouble with applying the force at the right angle
    consistently as the foot moves in the various quadrents.   There's
    something I need to fix with the trig, but I'm not figuring it out right
    now.
    
    Also added a force indicator sprite, but haven't done any of the scripts
    for it.

--- Jax/Assets/Scripts/FootControl.cs
+++ Jax/Assets/Scripts/FootControl.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class FootControl : MonoBehaviour {
 
+	public float power = .1f;
+
 	public Transform footTransform;
 	public Rigidbody2D footRB;
 
+	public float alpha;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +16,49 @@ public class FootControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		// This line should be in RotateFoot when I'm done debugging it
+			
+		alpha = Mathf.Atan ((this.transform.position.x - footTransform.position.x) / (this.transform.position.y - footTransform.position.y) ) ;
+
+		if (this.transform.position.y - footTransform.position.y >= 0)
+			alpha = -alpha;
+
+		print (alpha * 180 / Mathf.PI);
+
+
+		if (GlobalVariables.pControl) {
+
+
+			if (Input.GetKey("a")) {
+				RotateFoot (true);
+			}
+			if (Input.GetKey("d")) {
+				RotateFoot (false);
+			}
+
+
+		
+		
+		} 
+		else {
+			AutoControl ();
+			//print (Mathf.Cos (-45));
+		}
+
+	}
+
+	public void AutoControl () {
+
+
+	}
+
+	public void RotateFoot(bool left) {
+
+		if (left)
+			footRB.AddForce (new Vector2(Mathf.Cos(-alpha) * power, Mathf.Sin(-alpha) * power));
+		else
+			footRB.AddForce (new Vector2(Mathf.Cos(-alpha) * -power, Mathf.Sin(-alpha) * -power));
 	
 	}
 }
