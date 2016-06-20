f75a102ecc4508d752f1f45b8fb0e685f5233c2b
Rienzi Gokea <RGokea@gmail.com>
2016-06-19 21:37:27 -0400

    Slowed Air Movement
    Added a collider that differentiates when the player is on the ground or
    not so as to slow the airmovement.

--- Jax/Assets/Scripts/MoveBody.cs
+++ Jax/Assets/Scripts/MoveBody.cs
@@ -3,26 +3,50 @@ using System.Collections;
 
 public class MoveBody : MonoBehaviour {
 
-	public float moveSpeed = 50;
+	public float maxSpeed = 4.5f;
+	public float moveAcc = 50f;
+	public float airAcc = 10f;
+
+	public bool onGround;
 
 	private SpringControl sc;
 	private Rigidbody2D rb;
+	//private BoxCollider2D bc;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sc = GetComponent<SpringControl>();
+		//bc = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		if (sc.retracted) {
-			if (Input.GetKey("a") && rb.velocity.x >= -4.5f)
-				rb.AddForce(Vector2.left * moveSpeed, ForceMode2D.Force);
-			if (Input.GetKey("d") && rb.velocity.x <= 4.5f)
-				rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Force);
+			if (onGround) {
+				if (Input.GetKey ("a") && rb.velocity.x >= -maxSpeed)
+					rb.AddForce (Vector2.left * moveAcc, ForceMode2D.Force);
+				if (Input.GetKey ("d") && rb.velocity.x <= maxSpeed)
+					rb.AddForce (Vector2.right * moveAcc, ForceMode2D.Force);
+			} 
+			else {
+				if (Input.GetKey ("a") && rb.velocity.x >= -maxSpeed)
+					rb.AddForce (Vector2.left * airAcc, ForceMode2D.Force);
+				if (Input.GetKey ("d") && rb.velocity.x <= maxSpeed)
+					rb.AddForce (Vector2.right * airAcc, ForceMode2D.Force);
+			}
 		}
 
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Ground")
+			onGround = true;
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		if (other.gameObject.tag == "Ground")
+			onGround = false;
 	}
 }
