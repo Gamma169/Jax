47dec43089f3f544212f35b671b771a3a050d113
Rienzi Gokea <RGokea@gmail.com>
2016-06-24 02:34:05 -0400

    Made Action Switch
    I coded up an interaction switch and placed one in the main playground
    level.  Works well and is set up in such a way to power multiple things
    if need be as well as multiple types of objects.

---
+++ Jax/Assets/Scripts/ActionSwitch.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionSwitch : MonoBehaviour {
+
+	public bool active;
+	//Do we want it to be toggleable or only one-time use
+	public bool toggle;
+
+	public Color deactColor;
+	public Color activColor;
+
+	//I only have this so I don't have to constantly run a for loop.  I only want to run it once when the state has changed.
+	private bool stateChange;
+
+	// I made this an array so the switch can activate a bunch of things instead of just one.
+	public GameObject[] activatees;
+	private MovementActivator[] MAScripts;
+//	private SpringActivator[] SAScripts		//Curently I don't have any spring activators, but when I do, I can come back to this script and just add them quickly.
+
+	private SpriteRenderer SR;
+
+
+	// Use this for initialization
+	void Start () {
+		MAScripts = new MovementActivator[activatees.Length];
+		//SAScripts.Length = activatees.Length;
+		// Go through the array of activatable objects and get references to the activation scripts in those objects
+		for (int i=0; i<activatees.Length; i++) {
+			MAScripts[i] = activatees[i].GetComponent<MovementActivator>();
+			//SAScripts[i] = activatees[i].GetComponent<SpringActivator>();
+		}
+		SR = GetComponent<SpriteRenderer>();
+		stateChange = active;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Only do this if the state has changed since the last frame
+		if (stateChange != active) {
+			//Run through the arrays and change the states of the array elements
+			for (int i = 0; i < MAScripts.Length; i++) {
+				if (MAScripts[i])
+					MAScripts[i].scriptActive = active;
+				//if (MAScripts[i])
+				//SAScripts[i].scriptActive = active;
+			}
+			//Change the color
+			if (active)
+				SR.color = activColor;
+			else
+				SR.color = deactColor;
+			//Set the states to be the same
+			stateChange = active;
+		}
+			
+	}
+
+	// We only want to activate if the player is close enough
+	void OnTriggerStay2D(Collider2D other) {
+
+		if (Input.GetKeyDown("f") && other.gameObject.tag == "Player") {
+			if (toggle)
+				active = !active;
+			else
+				active = true;
+		}
+	
+	}
+}
---
+++ Jax/Assets/Scripts/MovementActivator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementActivator : MonoBehaviour {
+
+	public Color deactColor;
+	public Color activColor;
+
+	public bool scriptActive;
+
+	private MovingPlatform MPScript;
+	private SpriteRenderer SR;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		MPScript = GetComponent<MovingPlatform>();
+		SR = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MPScript.enabled = scriptActive;
+
+		if (scriptActive)
+			SR.color = activColor;
+		else {
+			SR.color = deactColor;
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
